67ade35fcdf4f3a56301b9e185b2584195c78858
Joost VandeVondele <Joost.VandeVondele@gmail.com>
2026-04-11 14:39:20 +0200

    Add -j to build.sh to limit number of processes used in building, and memory use

--- build.sh
+++ build.sh
@@ -4,7 +4,15 @@ BASE=$PWD
 OUTPUT=output
 mkdir -p $OUTPUT
 
+JOBS=$(nproc)
+
+while getopts "j:" opt; do
+  case $opt in
+  j) JOBS="$OPTARG" ;;
+  esac
+done
+
 git submodule update --init --recursive
 
 cd $BASE/$OUTPUT && cmake ../ -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$BASE/$OUTPUT -DWITH_TESTS=OFF -DWITH_TOOLS=OFF -DWITH_TERARK_ZIP=ON -DWITH_TBB=ON -DDISABLE_STALL_NOTIF=ON
-cd $BASE/$OUTPUT && make -j $(nproc) && make install > /dev/null 2>&1
+cd $BASE/$OUTPUT && make -j "$JOBS" && make install >/dev/null 2>&1
